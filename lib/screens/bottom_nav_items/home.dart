993e34e6c3ddcfe0695ad0cd6299d5f05542cad7
soumitrokarmaker <soumitrokarmaker@gmail.com>
2022-12-02 17:23:36 +0600

    6h commit

--- lib/screens/bottom_nav_items/home.dart
+++ lib/screens/bottom_nav_items/home.dart
@@ -8,6 +8,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _value = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,16 +16,58 @@ class _HomeState extends State<Home> {
         elevation: 0.0,
         backgroundColor: Colors.transparent,
         leading: IconButton(
-          icon: Icon(Icons.menu,color: Colors.blue,),
+          icon: Icon(
+            Icons.menu,
+            color: Colors.blue,
+          ),
           onPressed: () {},
         ),
         actions: [
-
-          IconButton(onPressed: () {}, icon: Icon(Icons.notifications, color: Colors.blue,)),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.notifications,
+                color: Colors.blue,
+              )),
         ],
       ),
-      body: Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20.0, right: 20.0,),
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 200,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10), color: Colors.blue),
+            ),
+            SizedBox(height: 10,),
+            Text(
+              'Who is the best player of India Cricket Team ?',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10,),
+            Radio(
+              value: 1,
+              groupValue: _value,
+              onChanged: (value) {
+                setState(() {
+                  _value = value!;
+                });
+              },
+            ),
+            Radio(
+              value: 2,
+              groupValue: _value,
+              onChanged: (value) {
+                setState(() {
+                  _value = value!;
+                });
+              },
+            ),
+
+          ],
+        ),
       ),
     );
   }
