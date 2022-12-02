6062d8f1829bd7c2aab0feb71aec2fab7e312ca1
soumitrokarmaker <soumitrokarmaker@gmail.com>
2022-12-03 00:24:46 +0600

    7th Commit

--- lib/screens/bottom_nav_items/home.dart
+++ lib/screens/bottom_nav_items/home.dart
@@ -31,42 +31,157 @@ class _HomeState extends State<Home> {
               )),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 20.0, right: 20.0,),
-        child: Column(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: 200,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10), color: Colors.blue),
-            ),
-            SizedBox(height: 10,),
-            Text(
-              'Who is the best player of India Cricket Team ?',
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10,),
-            Radio(
-              value: 1,
-              groupValue: _value,
-              onChanged: (value) {
-                setState(() {
-                  _value = value!;
-                });
-              },
-            ),
-            Radio(
-              value: 2,
-              groupValue: _value,
-              onChanged: (value) {
-                setState(() {
-                  _value = value!;
-                });
-              },
-            ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 20.0,
+            right: 20.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 200,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.blue),
+                child: Center(
+                    child: Text(
+                  'Slider show',
+                  style: TextStyle(fontSize: 30, color: Colors.white),
+                )),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Who is the best player of India Cricket Team ?',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 40,
+                width: MediaQuery.of(context).size.width / 1.5,
+                decoration: BoxDecoration(
+                    color: Colors.blue[100],
+                    borderRadius: BorderRadius.all(Radius.circular(30))),
+                child: Row(
+                  children: [
+                    Radio(
+                      value: 1,
+                      groupValue: _value,
+                      onChanged: (value) {
+                        setState(() {
+                          _value = value!;
+                        });
+                      },
+                    ),
+                    Text('MS Dhoni')
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 40,
+                width: MediaQuery.of(context).size.width / 1.5,
+                decoration: BoxDecoration(
+                    color: Colors.blue[100],
+                    borderRadius: BorderRadius.all(Radius.circular(30))),
+                child: Row(
+                  children: [
+                    Radio(
+                      value: 2,
+                      groupValue: _value,
+                      onChanged: (value) {
+                        setState(() {
+                          _value = value!;
+                        });
+                      },
+                    ),
+                    Text('Virat Kholi')
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 40,
+                width: MediaQuery.of(context).size.width / 1.5,
+                decoration: BoxDecoration(
+                    color: Colors.blue[100],
+                    borderRadius: BorderRadius.all(Radius.circular(30))),
+                child: Row(
+                  children: [
+                    Radio(
+                      value: 3,
+                      groupValue: _value,
+                      onChanged: (value) {
+                        setState(() {
+                          _value = value!;
+                        });
+                      },
+                    ),
+                    Text('Rohit Sharma'),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 40,
+                width: MediaQuery.of(context).size.width / 2,
+                decoration: BoxDecoration(
+                    color: Colors.blue[500],
+                    borderRadius: BorderRadius.all(Radius.circular(30))),
+                child: Center(
+                    child: Text(
+                  'See Result',
+                  style: TextStyle(color: Colors.white),
+                )),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 200,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.blue),
+                child: Center(
+                    child: Text(
+                  'Slider show',
+                  style: TextStyle(fontSize: 30, color: Colors.white),
+                )),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Mohil blast : Police find dump of 7000 mobile phones.',
+                      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Icon(Icons.more_vert),
 
-          ],
+                ],
+              ),
+
+              SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
         ),
       ),
     );
