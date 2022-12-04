e070eed3472c7cbb2c15c126e7f7f023b6b68416
soumitrokarmaker <soumitrokarmaker@gmail.com>
2022-12-04 21:15:18 +0600

    10th commit

--- lib/screens/bottom_nav_items/add_post.dart
+++ lib/screens/bottom_nav_items/add_post.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nb_utils/nb_utils.dart';
 
 class AddPost extends StatefulWidget {
   const AddPost({Key? key}) : super(key: key);
@@ -11,13 +12,104 @@ class _AddPostState extends State<AddPost> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('a'),
-          elevation: 0.0),
-      
-      body: Column(
-        children:  [
-       Container(height: MediaQuery.of(context),)
-        ],
+      appBar: AppBar(elevation: 0.0),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height / 3,
+                width: MediaQuery.of(context).size.width / 1.5,
+                decoration: BoxDecoration(
+                    color: Colors.blue[100],
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                    border: Border.all(width: 1.5, color: Colors.blue)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.add,
+                      color: Colors.grey,
+                      size: 50,
+                    ),
+                    Text(
+                      'Add Post Images',
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              AppTextField(
+                textFieldType: TextFieldType.NAME,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    label: Text('Add Heading')),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              AppTextField(
+                textFieldType: TextFieldType.NAME,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    label: Text('Add Tag')),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              AppTextField(
+                textFieldType: TextFieldType.NAME,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    label: Text('Catagory')),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              AppTextField(
+                textFieldType: TextFieldType.NAME,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    label: Text('Add Video Link')),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              AppTextField(
+                textFieldType: TextFieldType.NAME,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    label: Text('Write Article')),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                height: 50,
+                width: MediaQuery.of(context).size.width / 2,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.all(Radius.circular(30))),
+                child: Center(
+                  child: Text('Post',
+                    style: TextStyle(fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
